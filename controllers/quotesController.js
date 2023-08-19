d31eb8852479492807f112d9601bf83b674cd104
Michelle Harley <chell.hx@gmail.com>
2023-08-18 23:30:03 -0400

    all routes in quotes controller successful

--- controllers/quotesController.js
+++ controllers/quotesController.js
@@ -6,9 +6,9 @@ const {
   allQuotesByShow,
   allQuotesByCharacter,
   getQuotesById,
-  getQuotesFromShowById,
-  getQuotesFromCharacterById,
   createQuote,
+  createQuoteFromShow,
+  createQuoteFromCharacter,
   deleteQuoteById,
   updateQuoteById,
 } = require("../queries/quoteQueries");
@@ -17,7 +17,6 @@ const { validateQuote } = require("../validations/validateInput");
 
 router.get("/", async (req, res) => {
   const getAllQuotes = await allQuotes();
-  console.log(allQuotes);
   if (!getAllQuotes) {
     return res.status(500).json({ Error: "Server Error. Please try again." });
   } else {
@@ -25,7 +24,13 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/list", async (req, res) => {
+router.post("/", validateQuote, async (req, res) => {
+  const newQuote = await createQuote(req.body);
+  console.log(newQuote);
+  return res.json(newQuote);
+});
+
+router.get("/shows-quotes", async (req, res) => {
   const { showId } = req.params;
   const allQuotesInShow = await allQuotesByShow(showId);
   if (allQuotesInShow.length === 0) {
@@ -39,7 +44,7 @@ router.get("/list", async (req, res) => {
   }
 });
 
-router.get("/list", async (req, res) => {
+router.get("/characters-quotes", async (req, res) => {
   const { characterId } = req.params;
   const allQuotesFromCharacter = await allQuotesByCharacter(characterId);
   if (allQuotesFromCharacter.length === 0) {
@@ -53,11 +58,83 @@ router.get("/list", async (req, res) => {
   }
 });
 
-router.post("/list", validateQuote, async (req, res) => {
+router.post("/shows-quotes", validateQuote, async (req, res) => {
   const { showId } = req.params;
 
-  const newCharacter = await createCharacterFromShow(showId, req.body);
-  return res.json(newCharacter);
+  const newQuoteByShow = await createQuoteFromShow(showId, req.body);
+  return res.json(newQuoteByShow);
+});
+
+router.post("/characters-quotes", validateQuote, async (req, res) => {
+  const { characterId } = req.params;
+  const newQuoteByCharacter = await createQuoteFromCharacter(
+    characterId,
+    req.body
+  );
+  console.log(newQuoteByCharacter);
+  return res.json(newQuoteByCharacter);
+});
+
+router.put("/shows-quotes/:id", validateQuote, async (req, res) => {
+  const { id } = req.params;
+
+  const updatedQuote = await updateQuoteById(id, req.body);
+
+  if (updatedQuote.length === 0) {
+    return res.status(404).json({
+      Error: "PUT request unsuccessful.",
+      message:
+        "Quote Not Found! Please try again or enter a different quote id.",
+    });
+  } else {
+    return res.json(updatedQuote[0]);
+  }
+});
+
+router.delete("/shows-quotes/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedQuote = await deleteQuoteById(id);
+
+  if (deletedQuote.length === 0) {
+    return res.status(404).json({
+      Error: "DELETE request unsuccessful.",
+      message:
+        "Quote Not Found! Please try again or enter a different quote id.",
+    });
+  } else {
+    return res.json(deletedQuote[0]);
+  }
+});
+
+router.put("/characters-quotes/:id", validateQuote, async (req, res) => {
+  const { id } = req.params;
+
+  const updatedQuote = await updateQuoteById(id, req.body);
+
+  if (updatedQuote.length === 0) {
+    return res.status(404).json({
+      Error: "PUT request unsuccessful.",
+      message:
+        "Quote Not Found! Please try again or enter a different quote id.",
+    });
+  } else {
+    return res.json(updatedQuote[0]);
+  }
+});
+
+router.delete("/characters-quotes/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedQuote = await deleteQuoteById(id);
+
+  if (deletedQuote.length === 0) {
+    return res.status(404).json({
+      Error: "DELETE request unsuccessful.",
+      message:
+        "Quote Not Found! Please try again or enter a different quote id.",
+    });
+  } else {
+    return res.json(deletedQuote[0]);
+  }
 });
 
 router.get("/:id", async (req, res) => {
@@ -74,12 +151,6 @@ router.get("/:id", async (req, res) => {
   } else {
     return res.json(quote[0]);
   }
-});
-
-router.post("/", validateQuote, async (req, res) => {
-  const newQuote = await createQuote(req.body);
-  console.log(newQuote);
-  return res.json(newQuote);
 });
 
 router.delete("/:id", async (req, res) => {
--- queries/quoteQueries.js
+++ queries/quoteQueries.js
@@ -11,7 +11,7 @@ const allQuotes = async () => {
 
 const allQuotesByShow = async (showId) => {
   try {
-    const showQuote = db.any(
+    const showQuote = await db.any(
       "SELECT * FROM quotes WHERE show_id = $1 ORDER BY id ASC",
       showId
     );
@@ -22,7 +22,7 @@ const allQuotesByShow = async (showId) => {
 };
 const allQuotesByCharacter = async (characterId) => {
   try {
-    const characterQuote = db.any(
+    const characterQuote = await db.any(
       "SELECT * FROM quotes WHERE character_id = $1 ORDER BY id ASC",
       characterId
     );
@@ -36,30 +36,6 @@ const getQuotesById = async (id) => {
   try {
     const quote = await db.any(`SELECT * FROM quotes WHERE id = $1`, id);
     return quote;
-  } catch (e) {
-    return e;
-  }
-};
-
-const getQuotesFromShowById = async (showId, quoteId) => {
-  try {
-    const showQuotes = await db.any(
-      `SELECT show_id, character_id, quote FROM shows JOIN quotes ON shows.id = quotes.show_id WHERE shows.id = $1 AND quotes.id = $2`,
-      [showId, quoteId]
-    );
-    return showQuotes;
-  } catch (e) {
-    return e;
-  }
-};
-
-const getQuotesFromCharacterById = async (characterId, quoteId) => {
-  try {
-    const characterQuotes = await db.any(
-      `SELECT show_id, character_id, quote FROM characters JOIN quotes ON characters.id = quotes.character_id WHERE characters.id = $1 AND quotes.id = $2`,
-      [characterId, quoteId]
-    );
-    return characterQuotes;
   } catch (e) {
     return e;
   }
@@ -93,7 +69,7 @@ const createQuoteFromCharacter = async (characterId, data) => {
   try {
     const newQuote = await db.one(
       "INSERT INTO quotes (character_id, show_id, quote) VALUES ($1, $2, $3) RETURNING *",
-      [characterId, show_id, data.quote]
+      [characterId, data.show_id, data.quote]
     );
     return newQuote;
   } catch (e) {
@@ -131,8 +107,6 @@ module.exports = {
   allQuotesByShow,
   allQuotesByCharacter,
   getQuotesById,
-  getQuotesFromShowById,
-  getQuotesFromCharacterById,
   createQuote,
   createQuoteFromShow,
   createQuoteFromCharacter,
