46897059e36903de3b5827c91c703104711278f8
Michelle Harley <chell.hx@gmail.com>
2023-09-05 13:39:59 -0400

    broken eren image replaced

--- db/seed.sql
+++ db/seed.sql
@@ -24,7 +24,7 @@ INSERT INTO characters (show_id, name, status, power_lvl, image) VALUES
 (2, 'Kakashi Hatake', 'Jonin-Level Shinobi, Former Hokage', 9, 'https://media.tenor.com/_KsnH9YVT5QAAAAC/kakashi.gif'),
 (2, 'Sasuke Uchiha', 'Former Member of the Akatsuki', 10, 'https://media.tenor.com/e9tRTtV8vU0AAAAC/sasuke-naruto.gif'),
 (2, 'Itachi Uchiha', 'Former Captain of ANBU, Former Member of the Akatsuki, Hidden Leaf''s Martyr', 10, 'https://steamuserimages-a.akamaihd.net/ugc/156903851085964994/8419FCE240BD49DC66CD463271A200738AFBF266/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false'),
-(3, 'Eren Jaeger', 'Jaegerists, Former Member of the Survey Corps Special Operations Squad', 10, 'https://thumbs.gfycat.com/BiodegradableCanineFlea-size_restricted.gif'),
+(3, 'Eren Jaeger', 'Jaegerists, Former Member of the Survey Corps Special Operations Squad', 10, 'https://64.media.tumblr.com/e38a2295a59280ba4a278d6602904060/e69642369f6e7538-61/s500x750/d95c48e0a86de08034b6c564a7f1f031076e541b.gifv'),
 (3, 'Levi Ackerman', 'Captain of the Survey Corps Special Operations Squad', 10, 'https://gifdb.com/images/high/cool-captain-levi-ackerman-2r15qy70pntvwp4a.gif'),
 (4, 'Gojo Satoru', 'Special Grade Jujutsu Sorceror', 10, 'https://media.tenor.com/AUQrhGRWJ4EAAAAC/gojo-gojo-satoru.gif'),
 (4, 'Yuji Itadori', null, 8, 'https://media.tenor.com/f7uhDqZB6GAAAAAd/yuuji-itadori-itadori.gif'),
---
+++ models/data.model.js
@@ -0,0 +1,129 @@
+const { v4: uuidv4 } = require("uuid");
+
+module.exports = [
+  {
+    id: uuidv4(),
+    name: "Ichigo Kurosaki",
+    show: "Bleach",
+    quote:
+      "Don't break anyone's heart, they only have one. Break their bones, they have 206.",
+  },
+  {
+    id: uuidv4(),
+    name: "Hollow Ichigo",
+    show: "Bleach",
+    quote: "I am... Zangetsu!",
+  },
+  {
+    id: uuidv4(),
+    name: "Kisuke Urahara",
+    show: "Bleach",
+    quote:
+      "There's a lot of products that you can buy with money...but my love is the only limited product that only you can have.",
+  },
+  {
+    id: uuidv4(),
+    name: "Sosuke Aizen",
+    show: "Bleach",
+    quote:
+      "The betrayal you can see is trivial. What is truly frightening and much more lethal is the betrayal that you don't see.",
+  },
+  {
+    id: uuidv4(),
+    name: "Madara Uchiha",
+    show: "Naruto",
+    quote: "Be strong enough to be someone who even the demons would fear.",
+  },
+  {
+    id: uuidv4(),
+    name: "Kakashi Hatake",
+    show: "Naruto",
+    quote:
+      "To know what is right and choose to ignore it is the act of a coward.",
+  },
+  {
+    id: uuidv4(),
+    name: "Naruto Uzumaki",
+    show: "Naruto",
+    quote:
+      "I don't quit...I don't run, I never go back on my word. That's my ninja way.",
+  },
+];
+
+/** - - - QUOTES SHELF - - -
+ * -x- -x- -x- -x- -x- -x- -x- -x- -x- -x- BLEACH
+ *
+ * - - - - - - - - ICHIGO KUROSAKI - - - - - - - -
+ * "If miracles only happen once, what are they called the second time?"
+ * "It's meaningless to just live, and it's meaningless to just fight. I want to win."
+ * "The difference in ability, what about it? Do you think I should give up just because you're stronger than me?"
+ * "Become strong not just for your own sake, but for your friends."
+ *
+ * - - - - - - - - HOLLOW ICHIGO - - - - - - - -
+ * "I don’t know about Zangetsu, but I refuse to play horse to a king who’s weaker than me, someone who’s uncertainty and hesitation will end up getting us both cut to pieces. If you aren’t willing to assert your strength and retain the crown, then I’ll destroy you... Which means I’ll be king."
+ *
+ * - - - - - - - - KISUKE URAHARA - - - - - - - -
+ * "That wasn't very nice. I do believe you've killed my hat."
+ * [Urahara writes Ichigo a message in blood] "Please meet at Urahara shop immediately." [After Ichigo freaked out] "P.S: If you look at this message and think that it is the message of a person before his death... then you have no sense of humor."
+ * "Your transformation is not a disease. You were just given the key to the door in front of you. There's no need to know the cause. No need for sorrow or sadness. What comes next is up to you. You can use your keys to open the door...or lock it tight. And if you do choose to open it... You can still decide not to pass through."
+ * "Training hard, risking your life... what's the difference?"
+ * "A warrior who has lost his ability to fight is only going to be in the way."
+ * "Don’t use others as a reason for walking into death’s arms."
+ * "Yes, we have no destiny."
+ * "You give me too much credit. These days, I’m just a plain old candy-store owner."
+ * "Only those swallowed up by ignorance and fear who take false steps, shall plunge us into the waters called 'destiny'."
+ *
+ * - - - - - - - - SOSUKE AIZEN - - - - - - - -
+ * "I never ask them to trust me at all."
+ * "Don’t use such strong words. It’ll only make you look weak."
+ * "It’s very difficult for me to step on an ant without crushing it."
+ * "Ichigo Kurosaki, all of the battles you’ve fought were all part of my plan."
+ * "A victor must speak not of the current state of the world, but of how it should rightfully be."
+ * "Distance only has meaning in a fight between equals."
+ * "No matter what may happen… As long as you walk by my side… There shall be no enemy that can stand before us."
+ * -x- -x- -x- -x- -x- -x- -x- -x- -x- -x- NARUTO
+ *
+ * - - - - - - - - NARUTO UZUMAKI - - - - - - - -
+ * "Believe It."
+ * "Some day I'll come after all of the hate inside you as well."
+ * "Hard work is worthless for those that don’t believe in themselves."
+ * "I'll never go back on My Word. That's my Nindo: my ninja way!!"
+ *
+ * - - - - - - - - LADY TSUNADE - - - - - - - -
+ * "People become stronger because they have things they cannot forget. That's what you call growth."
+ *
+ * - - - - - - - - OBITO UCHIHA - - - - - - - -
+ * "The moment people come to know love, they run the risk of carrying hate."
+ * "No one cared who I was until I put on a mask."
+ * "You'll probably have to suffer again from now on... but don't change, keep going your way."
+ * "There is no such thing as hope, the concept of hope is nothing more than giving up, a word that holds no true meaning."
+ *
+ * - - - - - - - - GAARA - - - - - - - -
+ * "We walked through the darkness of the world. That's why we are able to see even a silver of light."
+ * "People cannot win against their loneliness."
+ *
+ * - - - - - - - - ITACHI UCHIHA - - - - - - - -
+ * "Those who forgive themselves, and are able to accept their true nature...They are the strong ones!"
+ * "Don't cry Sasuke. Your big brother is here to protect you, no matter what happens."
+ * "People's lives don't end when they die. It ends when they lose faith."
+ * “If you want to know who you are, you have to look at your real self and acknowledge what you see.”
+ * "You don't become the Hokage to be acknowledged by everyone. The one who is acknowledged by everyone becomes the Hokage."
+ * "Even the strongest of opponents always has a weakness."
+ * “It is not wise to judge others based on your own preconceptions and by their appearances.”
+ *
+ * - - - - - - - - KAKASHI HATAKE - - - - - - - -
+ * "People who abandon their comrades are worse than scum."
+ * “The hole in one’s heart gets filled by others around you.“
+ * “Sorry I’m late, I’m afraid I got lost on the path of life.”
+ * “If comrades that you trust gather around you, hope can take physical form and become visible. That's what I believe…”
+ *
+ * - - - - - - - - MADARA UCHIHA - - - - - - - -
+ * "People cannot show each other their true feelings. Fear, suspicion, and resentment never subside."
+ * "I'd be lying if I said things are going according to my plan, but beggars can't be choosers."
+ * "The Uchiha is a clan destined for revenge..."
+ * "Talking about peace whilst spilling blood, it's something that only humans can do."
+ * “Hatred is born to protect love."
+ *
+ * - - - - - - - - ROCK LEE - - - - - - - -
+ * "A here is not the one who never falls. He is the one who gets up again and again."
+ */
